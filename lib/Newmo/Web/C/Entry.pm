05377d381aba46dee1a0afdc26df6051826934ca
tokuhirom <tokuhirom@gmail.com>
2010-01-02 20:22:09 +0900

    handle error

--- lib/Newmo/Web/C/Entry.pm
+++ lib/Newmo/Web/C/Entry.pm
@@ -3,16 +3,20 @@ use Amon::Web::C;
 
 sub show {
     my ($class, $entry_id, $page_no) = @_;
-    my ($entry) = model('DB::Feed')->single(
+    my $entry = model('DB::Feed')->single(
         'entry' => { entry_id => $entry_id }
-    );
-    my ($entry_page) = model('DB::Feed')->single(
-        'entry_page' => { entry_id => $entry_id, page_no => $page_no },
     );
     my $entry_page_count = model('DB::Feed')->search_by_sql(
         q{SELECT COUNT(entry_id) as cnt FROM entry_page where entry_id=?},
         [$entry_id]
     )->first->cnt;
+    my $entry_page = model('DB::Feed')->single(
+        'entry_page' => { entry_id => $entry_id, page_no => $page_no },
+    );
+    unless ($entry_page) {
+        warn "cannot get entry_page";
+        return res_404();
+    }
 
     render(
         'entry.mt',
