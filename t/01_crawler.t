3ed4915ab94876e86f21f15360a2df8d189b2894
Tokuhiro Matsuno <tokuhirom@gmail.com>
2010-01-02 12:38:01 +0900

    rewrite test with Test::mysqld

--- t/01_crawler.t
+++ t/01_crawler.t
@@ -6,34 +6,22 @@ use File::Temp qw/tempfile/;
 use File::Spec;
 use Test::More;
 use HTML::Scrubber;
-
-my $db = Newmo::M::DB::Feed->new({
-    dsn => 'dbi:SQLite:',
-});
+use t::Utils;
 
 my ($dedup_fh, $dedup_fn) = tempfile();
 
-my $sql = slurp('sql/newmo.sqlite.sql');
-for my $s (split /;/, $sql) {
-    next if $s !~ /\S/;
-    $db->dbh->do($s) or die $db->dbh->error;
-}
+my $c = setup_standalone();
+my $db = $c->model('DB::Feed');
 
+note "setuped db";
 my $scrubber = HTML::Scrubber->new();
 my $crawler = Newmo::Crawler->new(
     db         => $db,
     dedup_file => $dedup_fn,
     scrubber   => $scrubber,
 );
+note "ready to run";
 $crawler->crawl('http://blog.livedoor.jp/dankogai/index.rdf');
 ok 'works';
 done_testing;
-
-sub slurp {
-    my $file = shift;
-    open my $fh, '<', $file or die $!;
-    my $content = do { local $/; <$fh> };
-    close $fh;
-    return $content;
-}
 
---
+++ t/Utils.pm
@@ -0,0 +1,48 @@
+package t::Utils;
+use strict;
+use warnings;
+use base qw/Exporter/;
+our @EXPORT = qw/setup_standalone setup_webapp/;
+use Newmo;
+use Newmo::Web;
+use DBI;
+use Test::mysqld;
+use Test::More;
+
+our $CONFIG = { };
+our $SCHEMA  = 'sql/newmo.mysql.sql';
+
+sub setup_standalone {
+    my $mysqld = setup_mysqld();
+    return Newmo->bootstrap(config => $CONFIG, mysqld => $mysqld);
+}
+
+sub setup_webapp {
+    my $mysqld = setup_mysqld();
+    Newmo::Web->to_app(config => $CONFIG, mysqld => $mysqld);
+}
+
+sub setup_mysqld {
+    my $mysqld = Test::mysqld->new(
+        my_cnf => {
+            'skip-networking' => '',    # no TCP socket
+        }
+    ) or plan skip_all => $Test::mysqld::errstr;
+    my $dbh = DBI->connect($mysqld->dsn()) or die;
+    for my $sql (split /;/, slurp($SCHEMA)) {
+        next unless $sql =~ /\S/;
+        $dbh->do("$sql") or die;
+    }
+    $CONFIG->{'M::DB::Feed'}->{dsn} = $mysqld->dsn;
+    return $mysqld;
+}
+
+sub slurp {
+    my $fname = shift;
+    open my $fh, '<:utf8', $fname or die "cannot open $fname: $!";
+    my $ret = do {local $/; <$fh>};
+    close $fh;
+    $ret;
+}
+
+1;
