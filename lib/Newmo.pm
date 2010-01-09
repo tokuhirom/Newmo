2e226a70d6b1941d14048fc21e0673ffa8882eb6
tokuhirom <tokuhirom@gmail.com>
2010-01-10 00:06:38 +0900

    use Amon's factory feature.

--- lib/Newmo.pm
+++ lib/Newmo.pm
@@ -1,3 +1,35 @@
 package Newmo;
 use Amon -base;
+use LWP::UserAgent::WithCache ();
+use HTML::Scrubber            ();
+
+__PACKAGE__->add_factory(
+    'HTML::Scrubber' => sub {
+        my ($c, $klass, $conf) = @_;
+        my $scrubber = HTML::Scrubber->new();
+           $scrubber->rules($conf->{rules});
+           $scrubber->default($conf->{default});
+           $scrubber;
+    }
+);
+__PACKAGE__->add_factory(
+    'LWP::UserAgent::WithCache' => sub {
+        my ($c, $klass, $conf ) = @_;
+        return LWP::UserAgent::WithCache->new(
+            $conf || +{}
+        );
+    }
+);
+__PACKAGE__->add_factory(
+    'Crawler' => sub {
+        my ($c, $klass, $conf, $dedup_file ) = @_;
+        Newmo::Crawler->new(
+            db         => $c->model('DB::Feed'),
+            ua         => $c->get('LWP::UserAgent::WithCache'),
+            scrubber   => $c->get('HTML::Scrubber'),
+            dedup_file => $dedup_file,
+        );
+    },
+);
+
 1;
--- script/crawler.pl
+++ script/crawler.pl
@@ -8,39 +8,26 @@ use File::Temp qw(tempfile);
 use Getopt::Long;
 use Newmo::Crawler;
 use Newmo::M::DB::Feed;
-use LWP::UserAgent::WithCache;
-use HTML::Scrubber;
 use Try::Tiny;
+use Newmo;
 
 my $conffile = 'config.pl';
 GetOptions(
     'c|config=s' => \$conffile,
 );
 
-my ($dedup_fh, $dedup_file) = tempfile();
-
 die "cannot read configuration file: $conffile" unless -f $conffile;
 my $conf = do $conffile;
-
-my $db = Newmo::M::DB::Feed->new($conf->{'M::DB::Feed'});
-my $ua = LWP::UserAgent::WithCache->new(
-    $conf->{'LWP::UserAgent::WithCache'} || +{}
-);
-my $scrubber = HTML::Scrubber->new();
-$scrubber->rules($conf->{'HTML::Scrubber'}->{rules});
-$scrubber->default($conf->{'HTML::Scrubber'}->{default});
-my $crawler = Newmo::Crawler->new(
-    db         => $db,
-    dedup_file => $dedup_file,
-    ua         => $ua,
-    scrubber   => $scrubber,
-);
 
 &main;exit;
 
 # -------------------------------------------------------------------------
 
 sub main {
+    my ($dedup_fh, $dedup_file) = tempfile();
+    my $c = Newmo->bootstrap(config => $conf);
+    my $crawler = $c->get('Crawler', $dedup_file);
+
     for my $feed (@{ $conf->{feeds} }) {
         try {
             $crawler->crawl($feed);
