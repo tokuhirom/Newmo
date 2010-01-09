5780dc6319fd52c3fdc739fcc174dbc3edb87575
tokuhirom <tokuhirom@gmail.com>
2010-01-10 00:11:39 +0900

    lazy load for web app.

--- lib/Newmo.pm
+++ lib/Newmo.pm
@@ -1,11 +1,10 @@
 package Newmo;
 use Amon -base;
-use LWP::UserAgent::WithCache ();
-use HTML::Scrubber            ();
 
 __PACKAGE__->add_factory(
     'HTML::Scrubber' => sub {
         my ($c, $klass, $conf) = @_;
+        Amon::Util::load_class($klass);
         my $scrubber = HTML::Scrubber->new();
            $scrubber->rules($conf->{rules});
            $scrubber->default($conf->{default});
@@ -15,15 +14,18 @@ __PACKAGE__->add_factory(
 __PACKAGE__->add_factory(
     'LWP::UserAgent::WithCache' => sub {
         my ($c, $klass, $conf ) = @_;
-        return LWP::UserAgent::WithCache->new(
+        Amon::Util::load_class($klass);
+        return $klass->new(
             $conf || +{}
         );
     }
 );
 __PACKAGE__->add_factory(
     'Crawler' => sub {
-        my ($c, $klass, $conf, $dedup_file ) = @_;
-        Newmo::Crawler->new(
+        my ($c, $name, $conf, $dedup_file ) = @_;
+        my $klass = "Newmo::Crawler";
+        Amon::Util::load_class($klass);
+        $klass->new(
             db         => $c->model('DB::Feed'),
             ua         => $c->get('LWP::UserAgent::WithCache'),
             scrubber   => $c->get('HTML::Scrubber'),
