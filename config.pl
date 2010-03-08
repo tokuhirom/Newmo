afb3da9583be7942d68e2b9c16e76b2d8d95c59b
tokuhirom <tokuhirom@gmail.com>
2010-03-08 23:47:41 +0900

    なんか feedburner つかうようになってたっぽい

--- config.pl
+++ config.pl
@@ -15,7 +15,8 @@ mkdir $cachedir unless -d $cachedir;
     feeds      => [
 #       'http://blog.livedoor.jp/dankogai/index.rdf',
         'http://b.hatena.ne.jp/entrylist?mode=rss&sort=hot&threshold=5',
-        'http://b.hatena.ne.jp/hotentry?mode=rss',
+        'http://feeds.feedburner.com/hatena/b/hotentry',
+        # 'http://b.hatena.ne.jp/hotentry?mode=rss',
         'http://b.hatena.ne.jp/hotentry/news/rss',
         'http://feeds.digg.com/digg/container/technology/popular.rss',
         'http://api.pathtraq.com/newsite',
