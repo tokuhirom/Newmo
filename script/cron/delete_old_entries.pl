a0193da39d84436ed5f6a688581de107178c8129
tokuhirom <tokuhirom@gmail.com>
2010-05-30 14:12:56 +0900

    自動で古い entry を削除する仕組みをいれた

---
+++ script/cron/delete_old_entries.pl
@@ -0,0 +1,12 @@
+use strict;
+use warnings;
+use Newmo;
+
+my $conffname = shift @ARGV;
+my $conf = do $conffname or die "cannot load configuration file";
+my $c = Newmo->bootstrap(config => $conf);
+
+my ($last_entry_id) = $c->db->dbh->selectrow_array(q{SELECT entry_id FROM entry ORDER BY entry_id DESC LIMIT 1});
+$c->db->dbh->do(q{DELETE FROM entry WHERE entry_id < ? - 10000}, {}, $last_entry_id);
+# entry_page table is removed automatically by FOREING KEY.
+
