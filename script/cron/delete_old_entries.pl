e76cd2d4e223c9c5df78162afc50904da544f3e0
tokuhirom <tokuhirom@gmail.com>
2010-09-30 11:52:55 +0900

    use Amon2 style

--- script/cron/delete_old_entries.pl
+++ script/cron/delete_old_entries.pl
@@ -2,9 +2,7 @@ use strict;
 use warnings;
 use Newmo;
 
-my $conffname = shift @ARGV;
-my $conf = do $conffname or die "cannot load configuration file: $conffname";
-my $c = Newmo->bootstrap(config => $conf);
+my $c = Newmo->bootstrap();
 
 my ($last_entry_id) = $c->db->dbh->selectrow_array(q{SELECT entry_id FROM entry ORDER BY entry_id DESC LIMIT 1});
 $c->db->dbh->do(q{DELETE FROM entry WHERE entry_id < ? - 10000}, {}, $last_entry_id);
