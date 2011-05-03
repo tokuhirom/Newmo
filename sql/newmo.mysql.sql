03752d42c7174659ac60100828e1f181e579c8b5
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-05-03 11:58:54 +0900

    utf8!

--- sql/newmo.mysql.sql
+++ sql/newmo.mysql.sql
@@ -3,7 +3,7 @@ create table feed (
     link text not null,
     title varchar(255),
     unique (link(255))
-) engine=innodb;
+) engine=innodb charset=utf8;
 
 create table entry (
     entry_id int primary key auto_increment,
@@ -14,7 +14,7 @@ create table entry (
     hatenabookmark_users int default 0,
     issued int,
     modified int
-) engine=innodb;
+) engine=innodb charset=utf8;
 create unique index feed_link on entry (feed_id, link(255));
 alter table entry add index (feed_id, entry_id);
 
@@ -23,7 +23,7 @@ create table entry_page (
     entry_id int not null,
     page_no int not null,
     body text
-) engine=innodb;
+) engine=innodb charset=utf8;
 create unique index entry_id_page_no on entry_page (entry_id, page_no);
 ALTER TABLE entry_page ADD FOREIGN KEY (entry_id) REFERENCES entry(entry_id) ON DELETE CASCADE;
 
